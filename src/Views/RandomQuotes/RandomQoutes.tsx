ffa1045e263f600d9c3322fc38cea5f8ef012213
Ruen Offerman <ruen_offerman@hotmail.com>
2022-05-09 01:14:16 +0200

    RandomQuoutes styling - implement fontAwesome icons

--- src/Views/RandomQuotes/RandomQoutes.tsx
+++ src/Views/RandomQuotes/RandomQoutes.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 import styled from 'styled-components';
 import { useFetchRadomQuote } from './useFetchRadomQuote';
 import { Quote } from '../../components/quote/Quotes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotate, faShare } from '@fortawesome/free-solid-svg-icons';
 
 const StyledRandomQuotes = styled.div`
   flex-direction: column;
@@ -13,9 +15,35 @@ const StyledTitle = styled.h2`
   color: #4d4d4d;
   flex: 1;
   font-size: 22px;
+  letter-spacing: 4px;
   font-weight: 800;
   text-align: center;
 `;
+
+const StyledActionContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 24px auto;
+`;
+
+const StyledIcon = styled(FontAwesomeIcon).attrs({
+  color: '#4d4d4d',
+})`
+  cursor: pointer;
+  margin: 0 12px;
+  opacity: 0.5;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+const StyledRefetchIcon = styled(StyledIcon).attrs({
+  icon: faRotate,
+})``;
+
+const StyledRPopularIcon = styled(StyledIcon).attrs({
+  icon: faShare,
+})``;
 
 export const RandomQuotes: FC = () => {
   const { isLoading, error, data, refetch } = useFetchRadomQuote();
@@ -25,23 +53,28 @@ export const RandomQuotes: FC = () => {
   }
 
   if (isLoading) {
-    return <h1 data-testid='loader'>Loading...</h1>;
+    return <StyledTitle data-testid='loader'>Loading...</StyledTitle>;
   }
 
   if (error) {
-    return <h1 data-testid='error'>Error...</h1>;
+    return <StyledTitle data-testid='error'>Error...</StyledTitle>;
   }
 
   if (!data) {
-    return <h1 data-testid='noData'>Guess there's really nothing to say...</h1>;
+    return (
+      <StyledTitle data-testid='noData'>
+        Guess there's really nothing to say...
+      </StyledTitle>
+    );
   }
 
   return (
     <StyledRandomQuotes>
-      <StyledTitle>Random</StyledTitle>
       <Quote author={data.author} text={data.quote} />
-
-      <button onClick={handleRefetch}>refetch</button>
+      <StyledActionContainer>
+        <StyledRefetchIcon onClick={handleRefetch} />
+        <StyledRPopularIcon />
+      </StyledActionContainer>
     </StyledRandomQuotes>
   );
 };
