8759b1e4e9a88f99dcadb761c79cd5b2df7650ba
Ruen Offerman <ruen_offerman@hotmail.com>
2022-05-09 11:27:31 +0200

    Extract statusIcon styles

--- src/components/statusIcon/StatusIcon.tsx
+++ src/components/statusIcon/StatusIcon.tsx
@@ -1,35 +1,10 @@
-import React, { FC, useCallback, useState } from 'react';
-import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { FC } from 'react';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-
-const StyledContainer = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  margin-top: 45px;
-`;
-
-const StyledIcon = styled(FontAwesomeIcon).attrs({
-  color: '#4d4d4d',
-})`
-  cursor: pointer;
-  margin: 0 12px;
-  opacity: 0.5;
-  &:hover {
-    opacity: 0.8;
-  }
-`;
-
-const StyledCaption = styled.p`
-  color: #4d4d4d;
-  flex: 1;
-  font-size: 12px;
-  letter-spacing: 4px;
-  font-weight: 800;
-  margin-top: 24px;
-  text-align: center;
-`;
+import {
+  StyledContainer,
+  StyledIcon,
+  StyledCaption,
+} from './statusIcon.styles';
 
 export const StatusIcon: FC<{
   pulse?: boolean;
